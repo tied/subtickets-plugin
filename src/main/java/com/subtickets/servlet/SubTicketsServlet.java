45301d4af3771e1734fb5d4633776dfcb5332a6f
Pavel Shepilov <pshepilov@amcbridge.com>
2018-01-25 23:01:14 +0200

    Split logic of creating monthly payment and fund payment sub-tasks Add init implementation of creating monthly payments

--- src/main/java/com/subtickets/servlet/SubTicketsServlet.java
+++ src/main/java/com/subtickets/servlet/SubTicketsServlet.java
@@ -4,6 +4,8 @@ import com.atlassian.jira.bc.issue.IssueService;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.config.SubTaskManager;
 import com.atlassian.jira.exception.CreateException;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.IssueInputParameters;
 import com.atlassian.jira.issue.IssueInputParametersImpl;
 import com.atlassian.jira.issue.MutableIssue;
 import com.atlassian.jira.issue.issuetype.IssueType;
@@ -14,7 +16,6 @@ import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.net.Request;
 import com.atlassian.sal.api.net.RequestFactory;
 import com.atlassian.sal.api.net.ResponseException;
-import com.atlassian.sal.api.user.UserProfile;
 import com.subtickets.roomers.Roomers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +26,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Named
 public class SubTicketsServlet extends HttpServlet {
@@ -50,8 +54,6 @@ public class SubTicketsServlet extends HttpServlet {
     @ComponentImport
     private SubTaskManager subTaskManager;
 
-    private Request<?, ?> roomersRequest;
-
     private IssueType fundIssueType;
 
     private IssueType monthlyPaymentType;
@@ -65,7 +67,6 @@ public class SubTicketsServlet extends HttpServlet {
         this.requestFactory = requestFactory;
         this.labelManager = labelManager;
         this.subTaskManager = subTaskManager;
-        this.roomersRequest = requestFactory.createRequest(Request.MethodType.GET, ROOMERS_URL);
         Collection<IssueType> issueTypes = ComponentAccessor.getConstantsManager().getAllIssueTypeObjects();
         issueTypes.stream().filter(issueType -> issueType.getName().equals("Fund"))
                 .findFirst()
@@ -77,36 +78,63 @@ public class SubTicketsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String referer = req.getHeader("referer");
-        String issueKey = req.getParameter("id");
-        UserProfile user = userManager.getRemoteUser(req);
-        ApplicationUser applicationUser = jiraUserManager.getUserByKey(user.getUserKey().getStringValue());
-        MutableIssue issue = issueService.getIssue(applicationUser, issueKey).getIssue();
+        ApplicationUser applicationUser = jiraUserManager.getUserByKey(userManager.getRemoteUser(req).getUserKey().getStringValue());
+        MutableIssue issue = issueService.getIssue(applicationUser, req.getParameter("id")).getIssue();
 
         try {
-            Roomers roomers = roomersRequest.executeAndReturn(new RoomersResponseHandler());
-            roomers.forEach((id, roomer) -> {
-                        IssueInputParametersImpl parameters = new IssueInputParametersImpl();
-                        parameters.setReporterId(applicationUser.getName());
-                        parameters.setProjectId(issue.getProjectObject().getId());
-                        parameters.setIssueTypeId(issue.getIssueTypeId());
-                        parameters.setSummary(issue.getSummary());
-                        IssueService.CreateValidationResult validationResult = issueService.validateSubTaskCreate(applicationUser, issue.getId(), parameters);
-                        if (validationResult.isValid()) {
-                            IssueService.IssueResult issueResult = issueService.create(applicationUser, validationResult);
-                            MutableIssue subTask = issueResult.getIssue();
-                            labelManager.addLabel(applicationUser, subTask.getId(), roomer.owned_Doors[0], true);
-                            try {
-                                subTaskManager.createSubTaskIssueLink(issue, subTask, applicationUser);
-                            } catch (CreateException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-            );
+            if (issue.getIssueType() == monthlyPaymentType) {
+                createMonthlyPaymentsSubIssues(applicationUser, issue);
+            } else if (issue.getIssueType() == fundIssueType) {
+                createFundPaymentSubIssues();
+            }
         } catch (ResponseException e) {
             e.printStackTrace();
         }
-        resp.sendRedirect(referer);
+
+        resp.sendRedirect(req.getHeader("referer"));
+    }
+
+    private void createMonthlyPaymentsSubIssues(ApplicationUser user, Issue parentIssue) throws ResponseException {
+        Roomers roomers = requestFactory.createRequest(Request.MethodType.GET, ROOMERS_URL).executeAndReturn(new RoomersResponseHandler());
+        List<String> doors = roomers.values()
+                .stream()
+                .map(roomer -> roomer.owned_Doors)
+                .flatMap(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+        doors.forEach(door -> {
+            IssueInputParameters parameters = generateIssueInputParameters(user, parentIssue)
+                    .setSummary(parentIssue.getSummary() + " " + door);
+            Issue subIssue = doCreateSubIssue(user, parentIssue, parameters);
+            if (subIssue != null) {
+                labelManager.addLabel(user, subIssue.getId(), door, true);
+            }
+        });
+    }
+
+    private void createFundPaymentSubIssues() {
+
+    }
+
+    private IssueInputParameters generateIssueInputParameters(ApplicationUser user, Issue issue) {
+        return new IssueInputParametersImpl()
+                .setReporterId(user.getName())
+                .setProjectId(issue.getProjectId())
+                .setIssueTypeId(issue.getIssueTypeId());
+    }
+
+    private Issue doCreateSubIssue(ApplicationUser user, Issue parentIssue, IssueInputParameters parameters) {
+        IssueService.CreateValidationResult validationResult = issueService.validateSubTaskCreate(user, parentIssue.getId(), parameters);
+        if (validationResult.isValid()) {
+            IssueService.IssueResult issueResult = issueService.create(user, validationResult);
+            MutableIssue subIssue = issueResult.getIssue();
+            try {
+                subTaskManager.createSubTaskIssueLink(parentIssue, subIssue, user);
+            } catch (CreateException e) {
+                e.printStackTrace();
+            }
+            return subIssue;
+        }
+        return null;
     }
 }
