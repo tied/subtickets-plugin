9da4977571c60173c60b358947e1a617babfa289
Pavel Shepilov <pshepilov@amcbridge.com>
2018-01-25 22:09:01 +0200

    Move accessed jira components to servlet fields, using DI

--- src/main/java/com/subtickets/servlet/SubTicketsServlet.java
+++ src/main/java/com/subtickets/servlet/SubTicketsServlet.java
@@ -2,10 +2,12 @@ package com.subtickets.servlet;
 
 import com.atlassian.jira.bc.issue.IssueService;
 import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.config.SubTaskManager;
 import com.atlassian.jira.exception.CreateException;
 import com.atlassian.jira.issue.IssueInputParametersImpl;
 import com.atlassian.jira.issue.MutableIssue;
 import com.atlassian.jira.issue.issuetype.IssueType;
+import com.atlassian.jira.issue.label.LabelManager;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.user.util.UserManager;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
@@ -23,6 +25,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
 
 @Named
 public class SubTicketsServlet extends HttpServlet {
@@ -41,19 +44,35 @@ public class SubTicketsServlet extends HttpServlet {
     @ComponentImport
     private RequestFactory<?> requestFactory;
 
-    private IssueType subIssueType;
+    @ComponentImport
+    private LabelManager labelManager;
+
+    @ComponentImport
+    private SubTaskManager subTaskManager;
+
+    private Request<?, ?> roomersRequest;
+
+    private IssueType fundIssueType;
+
+    private IssueType monthlyPaymentType;
 
     @Inject
-    public SubTicketsServlet(IssueService issueService, com.atlassian.sal.api.user.UserManager userManager, RequestFactory requestFactory) {
+    public SubTicketsServlet(IssueService issueService, com.atlassian.sal.api.user.UserManager userManager, RequestFactory requestFactory, LabelManager labelManager,
+                             SubTaskManager subTaskManager) {
         this.issueService = issueService;
         this.userManager = userManager;
         this.jiraUserManager = ComponentAccessor.getUserManager();
         this.requestFactory = requestFactory;
-        ComponentAccessor.getConstantsManager().getAllIssueTypeObjects()
-                .stream()
-                .filter(issueType -> issueType.getName().equals("Sub-task"))
+        this.labelManager = labelManager;
+        this.subTaskManager = subTaskManager;
+        this.roomersRequest = requestFactory.createRequest(Request.MethodType.GET, ROOMERS_URL);
+        Collection<IssueType> issueTypes = ComponentAccessor.getConstantsManager().getAllIssueTypeObjects();
+        issueTypes.stream().filter(issueType -> issueType.getName().equals("Fund"))
                 .findFirst()
-                .ifPresent(issueType -> subIssueType = issueType);
+                .ifPresent(issueType -> fundIssueType = issueType);
+        issueTypes.stream().filter(issueType -> issueType.getName().equals("Monthly Payment"))
+                .findFirst()
+                .ifPresent(issueType -> monthlyPaymentType = issueType);
     }
 
     @Override
@@ -64,21 +83,21 @@ public class SubTicketsServlet extends HttpServlet {
         ApplicationUser applicationUser = jiraUserManager.getUserByKey(user.getUserKey().getStringValue());
         MutableIssue issue = issueService.getIssue(applicationUser, issueKey).getIssue();
 
-        Request<?, ?> request = requestFactory.createRequest(Request.MethodType.GET, ROOMERS_URL);
         try {
-            Roomers roomers = request.executeAndReturn(new RoomersResponseHandler());
+            Roomers roomers = roomersRequest.executeAndReturn(new RoomersResponseHandler());
             roomers.forEach((id, roomer) -> {
                         IssueInputParametersImpl parameters = new IssueInputParametersImpl();
                         parameters.setReporterId(applicationUser.getName());
                         parameters.setProjectId(issue.getProjectObject().getId());
-                        parameters.setIssueTypeId(subIssueType.getId());
+                        parameters.setIssueTypeId(issue.getIssueTypeId());
                         parameters.setSummary(issue.getSummary());
                         IssueService.CreateValidationResult validationResult = issueService.validateSubTaskCreate(applicationUser, issue.getId(), parameters);
                         if (validationResult.isValid()) {
                             IssueService.IssueResult issueResult = issueService.create(applicationUser, validationResult);
                             MutableIssue subTask = issueResult.getIssue();
+                            labelManager.addLabel(applicationUser, subTask.getId(), roomer.owned_Doors[0], true);
                             try {
-                                ComponentAccessor.getSubTaskManager().createSubTaskIssueLink(issue, subTask, applicationUser);
+                                subTaskManager.createSubTaskIssueLink(issue, subTask, applicationUser);
                             } catch (CreateException e) {
                                 e.printStackTrace();
                             }
